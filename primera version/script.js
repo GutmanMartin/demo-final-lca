c96c6f72fe655c6c954eb49750c4bd913b8a60bd
Gutman <gutmanmartin@gmail.com>
2025-10-11 20:09:59 -0300

    added object functionality

--- primera version/script.js
+++ primera version/script.js
@@ -1,3 +1,5 @@
+const color = [".", ",", "-", "+", "*", "<", ">", "x", "X", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+
 const plasma = []
 //const pLines = document.querySelectorAll(".pLine")
 const pLines = []
@@ -7,11 +9,11 @@ const domBody = document.querySelector("body")
 let t = 0
 
 const WIDTH = 20
-const HEIGTH = 20
+const HEIGHT = 20
 
 
 
-for (let y = 0; y < HEIGTH; y++) {
+for (let y = 0; y < HEIGHT; y++) {
     plasma.push([])
     for (let x = 0; x < WIDTH; x++) {
         plasma[y].push(0)
@@ -25,22 +27,23 @@ for (let y = 0; y < HEIGTH; y++) {
 
 function update() {
     t++
-    for (let y = 0; y < HEIGTH; y++) {
+    for (let y = 0; y < HEIGHT; y++) {
         for (let x = 0; x < WIDTH; x++) {
-            plasma[y][x] = getSin(
+            let a = getSin(
                 x/16
                 + y/8 
-                + t / 10
-                + getSin(x/50)
-                - getSin(y/20 + t/100)
-            ) % 10
+                + t / 10ED C
+                //+ getSin(x/50)
+                //- getSin(y/20 + t/100)
+            ) 
+            plasma[y][x] = color[a]
 
         }
     }
 }
 
 function draw() {
-    for (let y = 0; y < HEIGTH; y++) {
+    for (let y = 0; y < HEIGHT; y++) {
         let line = ""
         for (let x = 0; x < WIDTH; x++) {
             line += plasma[y][x]
@@ -58,6 +61,13 @@ setInterval(() => {
 
 
 function getSin(x) {
-    return Math.floor((Math.sin(x) +1) * 5)
+    return Math.floor((Math.sin(x) +1) * color.length/2)
 }
 
+function returnColor(n) {
+    return color[map(n, 0, 255, 0, color.length)]
+}
+
+function map(value, start1, stop1, start2, stop2) {
+  return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+}
---
+++ segunda version/script.js
@@ -0,0 +1,119 @@
+class Plasma {
+    constructor(w, h, c, upd) {
+
+        this.plasma = []
+        this.pLines = []
+        this.WIDTH = w
+        this.HEIGHT = h
+        this.update = upd
+        this.color = c
+    
+        for (let y = 0; y < this.HEIGHT; y++) {
+            this.plasma.push([])
+            for (let x = 0; x < this.HEIGHT; x++) {
+                this.plasma[y].push(0)
+            }
+            const p = document.createElement("p")
+            p.classList.add("pLine")
+            domBody.appendChild(p)
+            this.pLines.push(p)
+        }
+    }
+
+    draw() {
+        for (let y = 0; y < this.HEIGHT; y++) {
+            let line = ""
+            for (let x = 0; x < this.WIDTH; x++) {
+                line += this.plasma[y][x]
+            }
+            this.pLines[y].textContent = line
+        }
+    }
+
+}
+
+
+const color = [".", ",", "-", "+", "*", "<", ">", "x", "X", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+
+
+const update1 = function () {
+    for (let y = 0; y < this.HEIGHT; y++) {
+            for (let x = 0; x < this.WIDTH; x++) {
+                let a = getSin(
+                    x/16
+                    + y/8 
+                    + t / 10
+                    //+ getSin(x/50)
+                    //- getSin(y/20 + t/100)
+                ) 
+                this.plasma[y][x] = this.color[a]
+
+            }
+        }
+}
+
+const update2 = function () {
+    for (let y = 0; y < this.HEIGHT; y++) {
+            for (let x = 0; x < this.WIDTH; x++) {
+                let a = getSin(
+                    x/16
+                    + y/8 
+                    + t / 10
+                    + getSin(x/50)
+                    //- getSin(y/20 + t/100)
+                ) 
+                this.plasma[y][x] = this.color[a]
+
+            }
+        }
+}
+
+
+//const pLines = document.querySelectorAll(".pLine")
+
+const domBody = document.querySelector("body")
+
+let t = 0
+
+const cuadros = [
+    new Plasma(20, 20, color, update1),
+    new Plasma(40, 30, color, update2)
+]
+
+
+function update() {
+    t++
+    for (let i = 0; i < cuadros.length; i++) {
+        cuadros[i].update();
+        
+    }
+    
+    
+}
+
+function draw() {
+    for (let i = 0; i < cuadros.length; i++) {
+        cuadros[i].draw();
+        
+    }
+}
+
+setInterval(() => {
+    update()
+    draw()
+
+}, 200)
+
+
+
+function getSin(x) {
+    return Math.floor((Math.sin(x) +1) * color.length/2)
+}
+
+function returnColor(n) {
+    return color[map(n, 0, 255, 0, color.length)]
+}
+
+function map(value, start1, stop1, start2, stop2) {
+  return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+}
